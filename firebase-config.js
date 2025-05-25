c64b41f1f73e7b9e611e0bba7c237d5c0787fbdc
sigurdha <sigurdha@yahoo.com>
2025-05-25 19:16:43 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -2,12 +2,12 @@
 const firebaseConfig = {
   apiKey: "AIzaSyAtS2K6Q2SKmnmZTDQK7YEPzfKp41JDfCQ",
   authDomain: "turnering-7ac64.firebaseapp.com",
-  databaseURL: "https://turnering-7ac64-default-rtdb.firebaseio.com",
+  databaseURL: "https://turnering-7ac64-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "turnering-7ac64",
   storageBucket: "turnering-7ac64.appspot.com",
   messagingSenderId: "942774569294",
   appId: "1:942774569294:web:c3b648d93d52f6770ef61f"
 };
 
-// Initialiser Firebase
 firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
