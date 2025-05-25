f50fd3657aba281be08bbbe588352cfc71595e3c
sigurdha <sigurdha@yahoo.com>
2025-05-25 14:34:23 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+document.getElementById('app').innerHTML = `
+    <p><strong>Språkvalg:</strong> Norsk | English</p>
+    <p><strong>Registrerte spillere:</strong> 4</p>
+    <ul>
+        <li>Spiller 1 vs Spiller 2 – <em>Ikke spilt</em></li>
+        <li>Spiller 3 vs Spiller 4 – <em>Ikke spilt</em></li>
+    </ul>
+    <p><strong>Registrer resultat:</strong> Klikk her</p>
+`;
